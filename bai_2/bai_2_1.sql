02dcf56f74c01c4adcce54dbda00ba56d5f33ffd
unknown <nnthanhqt@gmail.com>
2025-09-16 14:39:29 +0700

    nop bai tap

--- bai_2/bai_2_1.sql
+++ bai_2/bai_2_1.sql
@@ -1,50 +1,50 @@
 create database bai_2_1;
 use bai_2_1;
-create table phieuxuat (
+create table phieu_xuat (
 ma_phieu_xuat int primary key auto_increment,
 ngay_xuat date
 );
-create table phieunhap (
+create table phieu_nhap (
 ma_phieu_nhap int primary key auto_increment,
 ngay_nhap date 
 );
-create table nhacungcap (
+create table nha_cung_cap (
 ma_cung_cap int primary key auto_increment,
 ten_nha_cung_cap varchar(50),
 dia_chi varchar(50)
 );
-create table donhang (
+create table don_hang (
 ma_don_hang int primary key auto_increment,
 ngay_tao_don_hang date,
-foreign key (ma_don_hang) references nhacungcap (ma_cung_cap)
+foreign key (ma_don_hang) references nha_cung_cap (ma_cung_cap)
 );
-create table vattu (
+create table vat_tu (
 ma_vat_tu int primary key auto_increment,
 ten_vat_tu varchar(50)
 );
-create table phieuxuatvattu (
+create table phieu_xuat_vat_tu (
 phieu_xuat int,
 vat_tu int,
 primary key (phieu_xuat, vat_tu),
-foreign key (phieu_xuat) references phieuxuat (ma_phieu_xuat),
-foreign key (vat_tu) references vattu (ma_vat_tu)
+foreign key (phieu_xuat) references phieu_xuat (ma_phieu_xuat),
+foreign key (vat_tu) references vat_tu (ma_vat_tu)
 );
-create table phieunhapvattu (
+create table phieu_nhap_vat_tu (
 phieu_nhap int,
 vat_tu int,
 primary key (phieu_nhap, vat_tu),
-foreign key (phieu_nhap) references phieunhap (ma_phieu_nhap),
-foreign key (vat_tu) references vattu (ma_vat_tu)
+foreign key (phieu_nhap) references phieu_nhap (ma_phieu_nhap),
+foreign key (vat_tu) references vat_tu (ma_vat_tu)
 );
-create table donhangvattu (
+create table don_hang_vat_tu (
 don_hang int,
 vat_tu int,
 primary key (don_hang, vat_tu),
-foreign key (don_hang) references donhang (ma_don_hang),
-foreign key (vat_tu) references vattu (ma_vat_tu)
+foreign key (don_hang) references don_hang (ma_don_hang),
+foreign key (vat_tu) references vat_tu (ma_vat_tu)
 );
-create table sodienthoai (
+create table so_dien_thoai (
 so_dien_thoai varchar(20),
 nha_cung_cap int primary key,
-foreign key (nha_cung_cap) references nhacungcap (ma_cung_cap)
+foreign key (nha_cung_cap) references nha_cung_cap (ma_cung_cap)
 );
--- bai_2/bai_2_2.sql
+++ bai_2/bai_2_2.sql
@@ -1,26 +1,26 @@
-create database quanlybanhang;
-use quanlybanhang;
-create table khachhang (
-cID int primary key auto_increment,
-cName varchar(100),
-cAge int
+create database bai_2_2;
+use bai_2_2;
+create table khach_hang (
+ma_khach_hang int primary key auto_increment,
+ten_khach_hang varchar(100),
+tuoi_khach_hang int
 );
-create table hoadon (
-oID int primary key auto_increment,
-kCID int,
-oDate date,
-oTotalPrice int,
-foreign key (kCID) references khachhang(cID)
+create table hoa_don (
+ma_hoa_don int primary key auto_increment,
+khach_hang int,
+ngay_tao_hoa_don date,
+tong_gia int,
+foreign key (khach_hang) references khach_hang(ma_khach_hang)
 );
-create table sanpham (
-pID int primary key auto_increment,
-pName varchar(200),
-pPrice int
+create table san_pham (
+ma_san_pham int primary key auto_increment,
+ten_san_pham varchar(200),
+gia_san_pham int
 );
-create table hoadonsanpham (
-hOCID int,
-sPID int,
-odQTY varchar(100),
-foreign key (oOCID) references hoadon (oID),
-foreign key (sPID) references sanpham (pID)
+create table hoa_don_san_pham (
+hoa_don int,
+san_pham int,
+so_luong_dat_hang varchar(100),
+foreign key (hoa_don) references hoa_don (ma_hoa_don),
+foreign key (san_pham) references san_pham (ma_san_pham)
 );
